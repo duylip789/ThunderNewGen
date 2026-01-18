9da2d90771f0caf6cfe6401cc4cf0749c3d75216
duylip789 <duylip555@gmail.com>
2026-01-18 20:51:10 +0700

    d

--- src/main/java/thunder/hack/features/modules/combat/MaceSwap.java
+++ src/main/java/thunder/hack/features/modules/combat/MaceSwap.java
@@ -1,7 +1,8 @@
 package thunder.hack.features.modules.combat;
 
 import thunder.hack.features.modules.Module;
-import thunder.hack.features.setting.Setting;
+// Sửa dòng import này (thêm chữ s vào settings hoặc kiểm tra đúng đường dẫn của NextGen)
+import thunder.hack.setting.Setting; 
 import thunder.hack.utility.player.InventoryUtility;
 import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
@@ -9,12 +10,11 @@ import net.minecraft.item.AxeItem;
 
 public class MaceSwap extends Module {
 
-    // Đây là các dòng tạo ra Setting trong Menu
-    // Khi bạn chuột phải vào module, các dòng này sẽ hiện ra
-    private final Setting<Boolean> sword = new Setting<>("Sword", true); // Dung kiem khi dung dat
-    private final Setting<Boolean> axe = new Setting<>("Axe", false);   // Dung riu khi dung dat
-    private final Setting<Boolean> windBurst = new Setting<>("WindBurst", true); // Uu tien Mace co dong nay
-    private final Setting<Boolean> breach = new Setting<>("Breach", false);      // Uu tien Mace co dong nay
+    // Khai báo lại Setting cho đúng cú pháp NextGen
+    private final Setting<Boolean> sword = new Setting<>("Sword", true);
+    private final Setting<Boolean> axe = new Setting<>("Axe", false);
+    private final Setting<Boolean> windBurst = new Setting<>("WindBurst", true);
+    private final Setting<Boolean> breach = new Setting<>("Breach", false);
 
     public MaceSwap() {
         super("MaceSwap", "Tu dong doi sang Mace khi roi xuong", Category.COMBAT);
@@ -24,39 +24,21 @@ public class MaceSwap extends Module {
     public void onUpdate() {
         if (mc.player == null) return;
 
-        // Kiem tra xem nguoi choi co dang roi tu do khong (De dap Crit Mace)
-        boolean isFalling = mc.player.fallDistance > 0.5;
+        boolean isFalling = mc.player.fallDistance > 0.5f; // Thêm chữ f sau 0.5
 
         if (isFalling) {
-            // Neu dang roi -> Tim Mace de swap
-            int maceSlot = findMace();
+            int maceSlot = InventoryUtility.findItemInHotBar(Items.MACE);
             if (maceSlot != -1) {
                 InventoryUtility.switchTo(maceSlot);
             }
         } else {
-            // Neu dang dung duoi dat -> Tim Kiem hoac Riu
             if (sword.getValue()) {
-                int swordSlot = findSword();
+                int swordSlot = InventoryUtility.findClass(SwordItem.class);
                 if (swordSlot != -1) InventoryUtility.switchTo(swordSlot);
             } else if (axe.getValue()) {
-                int axeSlot = findAxe();
+                int axeSlot = InventoryUtility.findClass(AxeItem.class);
                 if (axeSlot != -1) InventoryUtility.switchTo(axeSlot);
             }
         }
-    }
-
-    // Ham tim Mace (Co xu ly logic WindBurst/Breach don gian)
-    private int findMace() {
-        // Trong ThunderHack thuc te ban can dung EnchantmentHelper de check WindBurst
-        // Day la code tim Mace co ban nhat:
-        return InventoryUtility.findItemInHotBar(Items.MACE);
-    }
-
-    private int findSword() {
-        return InventoryUtility.findClass(SwordItem.class);
-    }
-
-    private int findAxe() {
-        return InventoryUtility.findClass(AxeItem.class);
     }
 }
