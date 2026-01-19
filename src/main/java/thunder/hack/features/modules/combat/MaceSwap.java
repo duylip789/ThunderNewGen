41bce542d0c48c8488adf1444aeae74dbe001842
duylip789 <duylip555@gmail.com>
2026-01-19 12:23:07 +0700

    x

--- src/main/java/thunder/hack/features/modules/combat/MaceSwap.java
+++ src/main/java/thunder/hack/features/modules/combat/MaceSwap.java
@@ -1,44 +1,47 @@
 package thunder.hack.features.modules.combat;
 
 import thunder.hack.features.modules.Module;
-// Sửa dòng import này (thêm chữ s vào settings hoặc kiểm tra đúng đường dẫn của NextGen)
-import thunder.hack.setting.Setting; 
+import thunder.hack.setting.Setting;
 import thunder.hack.utility.player.InventoryUtility;
 import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
-import net.minecraft.item.AxeItem;
 
 public class MaceSwap extends Module {
 
-    // Khai báo lại Setting cho đúng cú pháp NextGen
-    private final Setting<Boolean> sword = new Setting<>("Sword", true);
-    private final Setting<Boolean> axe = new Setting<>("Axe", false);
-    private final Setting<Boolean> windBurst = new Setting<>("WindBurst", true);
-    private final Setting<Boolean> breach = new Setting<>("Breach", false);
+    // Tạo menu thả xuống (Mode)
+    private final Setting<Mode> mode = new Setting<>("Mode", Mode.WindBurst);
+    
+    // Các nút gạt tích (Checkboxes)
+    private final Setting<Boolean> maceStun = new Setting<>("Mace Stun", false);
+    private final Setting<Boolean> switchBack = new Setting<>("Switch Back", true);
+    private final Setting<Boolean> silent = new Setting<>("Silent", true);
+
+    // Khai báo các chế độ trong Mode
+    private enum Mode {
+        Normal, Breach, WindBurst
+    }
 
     public MaceSwap() {
-        super("MaceSwap", "Tu dong doi sang Mace khi roi xuong", Category.COMBAT);
+        super("MaceSwap", Category.COMBAT);
     }
+
+    private int oldSlot = -1;
 
     @Override
     public void onUpdate() {
         if (mc.player == null) return;
 
-        boolean isFalling = mc.player.fallDistance > 0.5f; // Thêm chữ f sau 0.5
-
-        if (isFalling) {
-            int maceSlot = InventoryUtility.findItemInHotBar(Items.MACE);
+        // Logic cơ bản: Nếu đang rơi thì swap sang Mace
+        if (mc.player.fallDistance > 0.5f) {
+            int maceSlot = InventoryUtility.findItemInHotBar(Items.MACE).getSlot();
             if (maceSlot != -1) {
+                if (oldSlot == -1) oldSlot = mc.player.getInventory().selectedSlot;
                 InventoryUtility.switchTo(maceSlot);
             }
-        } else {
-            if (sword.getValue()) {
-                int swordSlot = InventoryUtility.findClass(SwordItem.class);
-                if (swordSlot != -1) InventoryUtility.switchTo(swordSlot);
-            } else if (axe.getValue()) {
-                int axeSlot = InventoryUtility.findClass(AxeItem.class);
-                if (axeSlot != -1) InventoryUtility.switchTo(axeSlot);
-            }
+        } else if (switchBack.getValue() && oldSlot != -1) {
+            // Nếu tiếp đất và có bật Switch Back thì đổi về đồ cũ
+            InventoryUtility.switchTo(oldSlot);
+            oldSlot = -1;
         }
     }
 }
