85a6f977dfd7b3f93f4a7bd9cda943ffc9e6dc88
duylip789 <duylip555@gmail.com>
2026-01-20 22:47:35 +0700

    H

--- src/main/java/thunder/hack/core/manager/client/ModuleManager.java
+++ src/main/java/thunder/hack/core/manager/client/ModuleManager.java
@@ -66,7 +66,6 @@ public class ModuleManager implements IManager {
     public static ChorusExploit chorusExploit = new ChorusExploit();
     public static MoreKnockback moreKnockback = new MoreKnockback();
     public static AntiServerAdd antiServerAdd = new AntiServerAdd();
-    public static AntiLegitMiss antiLegitMiss = new AntiLegitMiss();
     public static AntiBallPlace antiBallPlace = new AntiBallPlace();
     public static TridentBoost tridentBoost = new TridentBoost();
     public static FixHP fixHP = new FixHP();
