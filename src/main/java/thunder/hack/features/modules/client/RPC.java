52472f169c393cab1495e45e9304d2c0ac791f94
duylip789 <duylip555@gmail.com>
2026-01-20 20:09:17 +0700

    H

--- src/main/java/thunder/hack/features/modules/client/RPC.java
+++ src/main/java/thunder/hack/features/modules/client/RPC.java
@@ -15,53 +15,30 @@ import thunder.hack.utility.discord.DiscordRichPresence;
 import java.io.*;
 import java.util.Objects;
 
-import static thunder.hack.features.modules.client.ClientSettings.isRu;
-
 public final class RPC extends Module {
-    private static final DiscordRPC rpc = DiscordRPC.INSTANCE;
+    // Trỏ vào LazyLoader để tránh lỗi build và crash trên Mobile
+    private static final DiscordRPC rpc = DiscordRPC.LazyLoader.INSTANCE;
+
     public static Setting<Mode> mode = new Setting<>("Picture", Mode.Recode);
     public static Setting<Boolean> showIP = new Setting<>("ShowIP", true);
     public static Setting<sMode> smode = new Setting<>("StateMode", sMode.Stats);
-    public static Setting<String> state = new Setting<>("State", "Beta? Recode? NextGen?");
+    public static Setting<String> state = new Setting<>("State", "ThunderHack Recode NextGen");
     public static Setting<Boolean> nickname = new Setting<>("Nickname", true);
+    
     public static DiscordRichPresence presence = new DiscordRichPresence();
     public static boolean started;
-    static String String1 = "none";
-    private final Timer timer_delay = new Timer();
     private static Thread thread;
-    String slov;
-    String[] rpc_perebor_en = {"Parkour", "Reporting cheaters", "Touching grass", "Asks how to bind", "Reporting bugs", "Watching Kilab"};
-    String[] rpc_perebor_ru = {"Паркурит", "Репортит читеров", "Трогает траву", "Спрашивает как забиндить", "Репортит баги", "Смотрит Флюгера"};
-    int randomInt;
 
     public RPC() {
         super("DiscordRPC", Category.CLIENT);
     }
 
-    public static void readFile() {
-        try {
-            File file = new File("ThunderHackRecode/misc/RPC.txt");
-            if (file.exists()) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                    while (reader.ready()) {
-                        String1 = reader.readLine();
-                    }
-                }
-            }
-        } catch (Exception ignored) {
-        }
+    public enum Mode {
+        Recode, Custom
     }
 
-    public static void WriteFile(String url1, String url2) {
-        File file = new File("ThunderHackRecode/misc/RPC.txt");
-        try {
-            file.createNewFile();
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                writer.write(url1 + "SEPARATOR" + url2 + '\n');
-            } catch (Exception ignored) {
-            }
-        } catch (Exception ignored) {
-        }
+    public enum sMode {
+        Stats, Custom, Version
     }
 
     @Override
@@ -70,7 +47,10 @@ public final class RPC extends Module {
         if (thread != null && !thread.isInterrupted()) {
             thread.interrupt();
         }
-        rpc.Discord_Shutdown();
+        // Kiểm tra null để không lỗi trên Mobile
+        if (rpc != null) {
+            rpc.Discord_Shutdown();
+        }
     }
 
     @Override
@@ -80,79 +60,59 @@ public final class RPC extends Module {
 
     public void startRpc() {
         if (isDisabled()) return;
+        
+        // NẾU LÀ MOBILE (RPC NULL), THOÁT LUÔN ĐỂ KHÔNG CRASH
+        if (rpc == null) return;
+
         if (!started) {
             started = true;
             DiscordEventHandlers handlers = new DiscordEventHandlers();
             rpc.Discord_Initialize("1093053626198523935", handlers, true, "");
             presence.startTimestamp = (System.currentTimeMillis() / 1000L);
-            presence.largeImageText = "v" + ThunderHack.VERSION + " [" + ThunderHack.GITHUB_HASH + "]";
+            presence.largeImageKey = "th_recode";
+            presence.largeImageText = "v" + ThunderHack.VERSION;
             rpc.Discord_UpdatePresence(presence);
 
             thread = new Thread(() -> {
                 while (!Thread.currentThread().isInterrupted()) {
+                    if (rpc == null) break;
+                    
                     rpc.Discord_RunCallbacks();
                     presence.details = getDetails();
 
                     switch (smode.getValue()) {
                         case Stats ->
-                                presence.state = "Hacks: " + Managers.MODULE.getEnabledModules().size() + " / " + Managers.MODULE.modules.size();
+                                presence.state = "Modules: " + Managers.MODULE.getEnabledModules().size() + " / " + Managers.MODULE.modules.size();
                         case Custom -> presence.state = state.getValue();
-                        case Version -> presence.state = "v" + ThunderHack.VERSION +" for mc 1.21";
+                        case Version -> presence.state = "v" + ThunderHack.VERSION;
                     }
 
-                    if (nickname.getValue()) {
-                        presence.smallImageText = "logged as - " + mc.getSession().getUsername();
+                    if (nickname.getValue() && mc.getSession() != null) {
+                        presence.smallImageText = "Logged as " + mc.getSession().getUsername();
                         presence.smallImageKey = "https://minotar.net/helm/" + mc.getSession().getUsername() + "/100.png";
-                    } else {
-                        presence.smallImageText = "";
-                        presence.smallImageKey = "";
                     }
-
-                    presence.button_label_1 = "Download";
-                    presence.button_url_1 = "https://github.com/Pan4ur/ThunderHack-Recode/";
-
-                    switch (mode.getValue()) {
-                        case Recode -> presence.largeImageKey = "https://i.imgur.com/yY0z2Uq.gif";
-                        case MegaCute ->
-                                presence.largeImageKey = "https://media1.tenor.com/images/6bcbfcc0be97d029613b54f97845bc59/tenor.gif?itemid=26823781";
-                        case Custom -> {
-                            readFile();
-                            presence.largeImageKey = String1.split("SEPARATOR")[0];
-                            if (!Objects.equals(String1.split("SEPARATOR")[1], "none")) {
-                                presence.smallImageKey = String1.split("SEPARATOR")[1];
-                            }
-                        }
-                    }
+                    
                     rpc.Discord_UpdatePresence(presence);
                     try {
-                        Thread.sleep(2000L);
-                    } catch (InterruptedException ignored) {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        break;
                     }
                 }
-            }, "TH-RPC-Handler");
+            }, "Discord-RPC-Callback");
             thread.start();
         }
     }
 
     private String getDetails() {
-        String result = "";
-
-        if (mc.currentScreen instanceof MultiplayerScreen || mc.currentScreen instanceof AddServerScreen || mc.currentScreen instanceof TitleScreen) {
-            if(timer_delay.passedMs(60 * 1000)){
-                randomInt = (int)(Math.random() * (5 - 0 + 1) + 0);
-                slov = isRu() ? rpc_perebor_ru[randomInt] : rpc_perebor_en[randomInt];
-                timer_delay.reset();
-            }
-            result = slov;
-        } else if (mc.getCurrentServerEntry() != null) {
-            result = isRu() ? (showIP.getValue() ? "Играет на " + mc.getCurrentServerEntry().address : "Играет на сервере") : (showIP.getValue() ? "Playing on " + mc.getCurrentServerEntry().address : "Playing on server");
-        } else if (mc.isInSingleplayer()) {
-            result = isRu() ? "Читерит в одиночке" : "SinglePlayer hacker";
+        if (mc.currentScreen instanceof TitleScreen) return "In Main Menu";
+        if (mc.currentScreen instanceof MultiplayerScreen || mc.currentScreen instanceof AddServerScreen) return "Selecting Server";
+        if (mc.world == null) return "Loading...";
+        
+        if (mc.isInSingleplayer()) return "Singleplayer";
+        if (showIP.getValue() && mc.getCurrentServerEntry() != null) {
+            return "Playing on " + mc.getCurrentServerEntry().address;
         }
-        return result;
+        return "Playing Multiplayer";
     }
-
-    public enum Mode {Custom, MegaCute, Recode}
-
-    public enum sMode {Custom, Stats, Version}
 }
