d8171f74d95eca97e88db540b1df8baa8d684ff0
duylip789 <duylip555@gmail.com>
2026-01-21 12:32:53 +0700

    Update MixinBlock.javaH

--- src/main/java/thunder/hack/injection/MixinBlock.java
+++ src/main/java/thunder/hack/injection/MixinBlock.java
@@ -3,7 +3,6 @@ package thunder.hack.injection;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.FireBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
@@ -20,27 +19,22 @@ import static thunder.hack.core.manager.IManager.mc;
 public abstract class MixinBlock {
 
     @Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
-    private static void shouldDrawSideHook(BlockState state, BlockView world, BlockPos pos, Direction side, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
-        if (ModuleManager.xray.isEnabled() && ModuleManager.xray.wallHack.getValue())
-            cir.setReturnValue(XRay.isCheckableOre(state.getBlock()));
-        if(ModuleManager.autoAnchor.isEnabled() && state.getBlock() instanceof FireBlock)
-            cir.setReturnValue(false);
-    }
+    private static void shouldDrawSideHook(BlockState state, BlockView world,
+                                           BlockPos pos, Direction side,
+                                           BlockPos blockPos,
+                                           CallbackInfoReturnable<Boolean> cir) {
 
-    /*
-    @Inject(method = "isTransparent", at = @At("HEAD"), cancellable = true)
-    public void isTransparentHook(BlockState state, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (MinecraftClient.getInstance() == null) return;
-        if (ModuleManager.xray.isEnabled() && ModuleManager.xray.wallHack.getValue())
-            cir.setReturnValue(!XRay.isCheckableOre(state.getBlock()));
+        if (ModuleManager.xray.isEnabled() && ModuleManager.xray.wallHack.getValue()) {
+            cir.setReturnValue(XRay.isCheckableOre(state.getBlock()));
+        }
     }
-     */
 
     @Inject(method = "getVelocityMultiplier", at = @At("HEAD"), cancellable = true)
     public void getVelocityMultiplierHook(CallbackInfoReturnable<Float> cir) {
         if (ModuleManager.noSlow.isEnabled()) {
             if (ModuleManager.noSlow.soulSand.getValue() && (Object) this == Blocks.SOUL_SAND)
                 cir.setReturnValue(Blocks.DIRT.getVelocityMultiplier());
+
             if (ModuleManager.noSlow.honey.getValue() && (Object) this == Blocks.HONEY_BLOCK)
                 cir.setReturnValue(Blocks.DIRT.getVelocityMultiplier());
         }
@@ -51,8 +45,16 @@ public abstract class MixinBlock {
         if (ModuleManager.noSlow.isEnabled()) {
             if (ModuleManager.noSlow.slime.getValue() && (Object) this == Blocks.SLIME_BLOCK)
                 cir.setReturnValue(Blocks.DIRT.getSlipperiness());
-            if (ModuleManager.noSlow.ice.getValue() && ((Object) this == Blocks.ICE || (Object) this == Blocks.PACKED_ICE || (Object) this == Blocks.BLUE_ICE || (Object) this == Blocks.FROSTED_ICE) && !mc.options.jumpKey.isPressed())
+
+            if (ModuleManager.noSlow.ice.getValue()
+                    && ((Object) this == Blocks.ICE
+                    || (Object) this == Blocks.PACKED_ICE
+                    || (Object) this == Blocks.BLUE_ICE
+                    || (Object) this == Blocks.FROSTED_ICE)
+                    && !mc.options.jumpKey.isPressed()) {
+
                 cir.setReturnValue(Blocks.DIRT.getSlipperiness());
+            }
         }
     }
 }
