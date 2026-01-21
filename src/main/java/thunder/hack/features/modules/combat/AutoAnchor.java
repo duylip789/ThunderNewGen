b0e62155091266074ba0859ebb1f07ec469d0a69
duylip789 <duylip555@gmail.com>
2026-01-21 17:02:17 +0700

    H

--- src/main/java/thunder/hack/features/modules/combat/AutoAnchor.java
+++ src/main/java/thunder/hack/features/modules/combat/AutoAnchor.java
@@ -412,7 +412,7 @@ public final class AutoAnchor extends Module {
         if (ModuleManager.autoTrap.isEnabled() && !ModuleManager.surround.inactivityTimer.passedMs(500))
             return true;
 //
-            return false;
+         //   return false;
 //
         if (ModuleManager.holeFill.isEnabled() && !HoleFill.inactivityTimer.passedMs(500))
             return true;
