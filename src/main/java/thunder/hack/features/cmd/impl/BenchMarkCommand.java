c475f1647e7810855b1a1e113d6c305ac9416a1e
duylip789 <duylip555@gmail.com>
2026-01-21 12:16:30 +0700

    Update BenchMarkCommand.javav

--- src/main/java/thunder/hack/features/cmd/impl/BenchMarkCommand.java
+++ src/main/java/thunder/hack/features/cmd/impl/BenchMarkCommand.java
@@ -6,7 +6,6 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import thunder.hack.features.cmd.Command;
-import thunder.hack.utility.world.ExplosionUtility;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 import static thunder.hack.features.modules.client.ClientSettings.isRu;
