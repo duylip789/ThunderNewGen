6351dd05e12fda29ef5dc067f92ffef89ba521b1
duylip789 <duylip555@gmail.com>
2026-01-23 20:46:31 +0700

    G

--- src/main/java/thunder/hack/core/manager/client/ModuleManager.java
+++ src/main/java/thunder/hack/core/manager/client/ModuleManager.java
@@ -238,7 +238,7 @@ public class ModuleManager implements IManager {
     public static XRay xray = new XRay();
     public static Step step = new Step();
     public static Aura aura = new Aura();
-    public static TargetESP targetESP = new TargetESP();
+   // public static TargetESP targetESP = new TargetESP();
     public static FOV fov = new FOV();
     public static ESP esp = new ESP();
     public static RPC rpc = new RPC();
