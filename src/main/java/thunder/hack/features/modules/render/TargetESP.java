4e181f4ddf75d32ba6823990f332f4e67467b84b
duylip789 <duylip555@gmail.com>
2026-01-23 19:29:33 +0700

    G

--- src/main/java/thunder/hack/features/modules/render/TargetESP.java
+++ src/main/java/thunder/hack/features/modules/render/TargetESP.java
@@ -1,55 +1,74 @@
 package thunder.hack.features.modules.render;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Box;
+import net.minecraft.util.math.RotationAxis;
+import org.lwjgl.opengl.GL11;
 import thunder.hack.features.modules.Module;
-import thunder.hack.features.modules.combat.Aura;
-import thunder.hack.utility.render.Render2DEngine;
-import thunder.hack.utility.render.Render3DEngine;
 import thunder.hack.core.manager.client.ModuleManager;
 import thunder.hack.setting.Setting;
 import java.awt.Color;
 
 public class TargetESP extends Module {
 
-    private final Setting<Mode> mode = new Setting<>("Mode", Mode.Ghost);
     private final Setting<Color> color = new Setting<>("Color", new Color(255, 255, 255, 200));
     private final Setting<Float> size = new Setting<>("Size", 1.0f, 0.1f, 2.0f);
 
     public TargetESP() {
-        // Fix constructor 2 tham số cho 1.21
         super("TargetESP", Category.RENDER);
-    }
-
-    private enum Mode {
-        Circle, Cube, Ghost
     }
 
     @Override
     public void onRender3D(MatrixStack stack) {
-        // Kiểm tra an toàn để tránh ClassNotFound/NoClassDefFound
-        if (ModuleManager.aura == null) return;
-        
+        [span_0](start_span)[span_1](start_span)// Kiểm tra an toàn để tránh NoClassDefFoundError như trong log[span_0](end_span)[span_1](end_span)
+        if (ModuleManager.aura == null || ModuleManager.aura.target == null) return;
+
         Entity target = ModuleManager.aura.target;
-        if (target == null) return;
+        renderTargetCircle(stack, target);
+    }
 
+    private void renderTargetCircle(MatrixStack stack, Entity entity) {
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableCull();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+
+        stack.push();
+        
+        // Di chuyển matrix đến vị trí của target
+        double x = entity.lastRenderX + (entity.getX() - entity.lastRenderX) * mc.getTickDelta() - mc.getEntityRenderDispatcher().camera.getPos().x;
+        double y = entity.lastRenderY + (entity.getY() - entity.lastRenderY) * mc.getTickDelta() - mc.getEntityRenderDispatcher().camera.getPos().y;
+        double z = entity.lastRenderZ + (entity.getZ() - entity.lastRenderZ) * mc.getTickDelta() - mc.getEntityRenderDispatcher().camera.getPos().z;
+        
+        stack.translate(x, y + 0.1, z);
+        
+        // Hiệu ứng xoay tròn cho gl4es mượt hơn
+        float speed = (float) (System.currentTimeMillis() % 2000) / 2000f;
+        stack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(speed * 360f));
+
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferBuilder = tessellator.getBuffer();
+        
+        // Bắt đầu vẽ vòng tròn bằng GL_LINE_STRIP (tương thích gl4es tốt nhất)
+        bufferBuilder.begin(VertexFormat.DrawMode.LINE_STRIP, VertexFormats.POSITION_COLOR);
+        
         Color c = color.getValue();
+        float radius = size.getValue();
 
-        switch (mode.getValue()) {
-            case Circle:
-                // FIX: Chuyển c.getRGB() về int và thêm đủ 7 tham số theo log yêu cầu
-                Render3DEngine.drawCircle3D(stack, target, size.getValue(), c.getRGB(), 30, false, 1);
-                break;
-            case Cube:
-                Box box = target.getBoundingBox();
-                // Sử dụng render hộp 1.21
-                Render3DEngine.drawFilledBox(stack, box, Render2DEngine.injectAlpha(c, 100));
-                Render3DEngine.drawBoxOutline(box, c, 2.0f);
-                break;
-            case Ghost:
-                Render3DEngine.drawTargetEsp(stack, target);
-                break;
+        for (int i = 0; i <= 360; i += 5) {
+            double radians = Math.toRadians(i);
+            double circleX = Math.cos(radians) * radius;
+            double circleZ = Math.sin(radians) * radius;
+            bufferBuilder.vertex(stack.peek().getPositionMatrix(), (float)circleX, 0, (float)circleZ)
+                    .color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha()).next();
         }
+
+        tessellator.draw();
+        
+        stack.pop();
+        RenderSystem.enableCull();
+        RenderSystem.disableBlend();
     }
 }
