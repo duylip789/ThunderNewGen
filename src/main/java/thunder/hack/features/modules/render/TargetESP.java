09b96008732584ef9f431b503483d655cb8014f5
duylip789 <duylip555@gmail.com>
2026-01-23 19:23:01 +0700

    H

--- src/main/java/thunder/hack/features/modules/render/TargetESP.java
+++ src/main/java/thunder/hack/features/modules/render/TargetESP.java
@@ -9,7 +9,7 @@ import thunder.hack.utility.render.Render2DEngine;
 import thunder.hack.utility.render.Render3DEngine;
 import thunder.hack.core.manager.client.ModuleManager;
 import thunder.hack.setting.Setting;
-import java.awt.*;
+import java.awt.Color;
 
 public class TargetESP extends Module {
 
@@ -18,7 +18,7 @@ public class TargetESP extends Module {
     private final Setting<Float> size = new Setting<>("Size", 1.0f, 0.1f, 2.0f);
 
     public TargetESP() {
-        // FIX LỖI 1: Constructor chỉ nhận 2 tham số (String, Category)
+        // Fix constructor 2 tham số cho 1.21
         super("TargetESP", Category.RENDER);
     }
 
@@ -28,6 +28,9 @@ public class TargetESP extends Module {
 
     @Override
     public void onRender3D(MatrixStack stack) {
+        // Kiểm tra an toàn để tránh ClassNotFound/NoClassDefFound
+        if (ModuleManager.aura == null) return;
+        
         Entity target = ModuleManager.aura.target;
         if (target == null) return;
 
@@ -35,16 +38,16 @@ public class TargetESP extends Module {
 
         switch (mode.getValue()) {
             case Circle:
-                // FIX LỖI 2: drawCircle3D yêu cầu: MatrixStack, Entity, float radius, int color, int segments, boolean filled, int fade
+                // FIX: Chuyển c.getRGB() về int và thêm đủ 7 tham số theo log yêu cầu
                 Render3DEngine.drawCircle3D(stack, target, size.getValue(), c.getRGB(), 30, false, 1);
                 break;
             case Cube:
                 Box box = target.getBoundingBox();
+                // Sử dụng render hộp 1.21
                 Render3DEngine.drawFilledBox(stack, box, Render2DEngine.injectAlpha(c, 100));
                 Render3DEngine.drawBoxOutline(box, c, 2.0f);
                 break;
             case Ghost:
-                // Chế độ Ghost gọi thẳng hàm bạn đã có trong Render3DEngine
                 Render3DEngine.drawTargetEsp(stack, target);
                 break;
         }
