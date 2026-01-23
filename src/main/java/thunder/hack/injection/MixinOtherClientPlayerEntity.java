f5d8d64e15df10965517c916084175ad264d23de
duylip789 <duylip555@gmail.com>
2026-01-23 12:15:26 +0700

    Update MixinOtherClientPlayerEntity.java

--- src/main/java/thunder/hack/injection/MixinOtherClientPlayerEntity.java
+++ src/main/java/thunder/hack/injection/MixinOtherClientPlayerEntity.java
@@ -44,9 +44,9 @@ public class MixinOtherClientPlayerEntity extends AbstractClientPlayerEntity imp
                 }
             }
             if(bestPos != null) {
-                setPosition(bestPos.getX(), bestPos.getY(), bestPos.getZ());
+               // setPosition(bestPos.getX(), bestPos.getY(), bestPos.getZ());
                 if(Aura.target == this)
-                    ModuleManager.aura.resolvedBox = getBoundingBox();
+                    //ModuleManager.aura.resolvedBox = getBoundingBox();
             }
             return;
         }
@@ -54,17 +54,17 @@ public class MixinOtherClientPlayerEntity extends AbstractClientPlayerEntity imp
         Vec3d from = new Vec3d(((IEntityLiving) this).getPrevServerX(), ((IEntityLiving) this).getPrevServerY(), ((IEntityLiving) this).getPrevServerZ());
         Vec3d to = new Vec3d(serverX, serverY, serverZ);
 
-        if(mode == Aura.Resolver.Advantage) {
+        //if(mode == Aura.Resolver.Advantage) {
             if (mc.player.squaredDistanceTo(from) > mc.player.squaredDistanceTo(to)) setPosition(to.x, to.y, to.z);
             else setPosition(from.x, from.y, from.z);
         } else {
             setPosition(to.x, to.y, to.z);
         }
         if(Aura.target == this)
-            ModuleManager.aura.resolvedBox = getBoundingBox();
+            //ModuleManager.aura.resolvedBox = getBoundingBox();
     }
 
-    public void releaseResolver() {
+    public void releaseResolver(Object mode) {
         if (backUpY != -999) {
             setPosition(backUpX, backUpY, backUpZ);
             backUpY = -999;
