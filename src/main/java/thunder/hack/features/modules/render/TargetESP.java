8ae635332de21dd7f681fa84aba691527f463d0d
duylip789 <duylip555@gmail.com>
2026-01-23 18:59:56 +0700

    B

--- src/main/java/thunder/hack/features/modules/render/TargetESP.java
+++ src/main/java/thunder/hack/features/modules/render/TargetESP.java
@@ -4,39 +4,36 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Box;
 import thunder.hack.features.modules.Module;
-import thunder.hack.features.modules.client.HudEditor;
 import thunder.hack.features.modules.combat.Aura;
 import thunder.hack.utility.render.Render2DEngine;
 import thunder.hack.utility.render.Render3DEngine;
 import thunder.hack.core.manager.client.ModuleManager;
 import thunder.hack.setting.Setting;
-import thunder.hack.events.impl.WorldRenderEvent; // Ở 1.21 thường dùng WorldRenderEvent
 import java.awt.*;
 
 public class TargetESP extends Module {
-
-    public TargetESP() {
-        super("TargetESP", "Hien thi hieu ung quanh muc tieu", Category.RENDER);
-    }
 
     private final Setting<Mode> mode = new Setting<>("Mode", Mode.Ghost);
     private final Setting<Color> color = new Setting<>("Color", new Color(255, 255, 255, 200));
     private final Setting<Float> size = new Setting<>("Size", 1.0f, 0.1f, 2.0f);
 
+    public TargetESP() {
+        super("TargetESP", "Hien thi Target", Category.RENDER);
+    }
+
     private enum Mode {
         Circle, Cube, Ghost
     }
 
-    // Ở 1.21, hàm render 3D thường được đặt tên là onWorldRender hoặc onRender3D tùy mixin
+    // Ở bản 1.21 NewGen, Module đã có sẵn hàm onRender3D 
+    // Chúng ta không cần import EventRender3D hay WorldRenderEvent nữa
     @Override
-    public void onWorldRender(WorldRenderEvent event) { 
+    public void onRender3D(MatrixStack stack) {
         // Lấy mục tiêu từ Aura
-        Entity target = ModuleManager.targetESP.getTarget(); // Hoặc dùng cách cũ của bạn
-        if (target == null) target = ModuleManager.aura.target;
+        Entity target = ModuleManager.aura.target;
         
         if (target == null) return;
 
-        MatrixStack stack = event.getStack(); // 1.21 dùng getStack() thay vì getMatrixStack()
         Color c = color.getValue();
 
         switch (mode.getValue()) {
@@ -49,13 +46,9 @@ public class TargetESP extends Module {
                 Render3DEngine.drawBoxOutline(box, c, 2.0f);
                 break;
             case Ghost:
+                // Hàm này bạn đã có trong Render3DEngine.java đã upload
                 Render3DEngine.drawTargetEsp(stack, target);
                 break;
         }
-    }
-
-    // Hàm bổ trợ để lấy target an toàn
-    public Entity getTarget() {
-        return ModuleManager.aura.isEnabled() ? ModuleManager.aura.target : null;
     }
 }
