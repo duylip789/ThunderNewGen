b3600fb6d7c44924c5039f98537b253954d0eade
duylip789 <duylip555@gmail.com>
2026-01-23 13:31:53 +0700

    V

--- src/main/java/thunder/hack/injection/MixinOtherClientPlayerEntity.java
+++ src/main/java/thunder/hack/injection/MixinOtherClientPlayerEntity.java
@@ -1,44 +1,25 @@
 package thunder.hack.injection;
 
-import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import thunder.hack.features.modules.misc.FakePlayer;
-import thunder.hack.utility.interfaces.IOtherClientPlayerEntity;
+import thunder.hack.injection.interfaces.IOtherClientPlayerEntity;
 
 @Mixin(OtherClientPlayerEntity.class)
-public class MixinOtherClientPlayerEntity extends AbstractClientPlayerEntity implements IOtherClientPlayerEntity {
-    @Unique private double backUpX, backUpY, backUpZ;
+public abstract class MixinOtherClientPlayerEntity extends AbstractClientPlayerEntity implements IOtherClientPlayerEntity {
 
-    public MixinOtherClientPlayerEntity(ClientWorld world, GameProfile profile) {
+    public MixinOtherClientPlayerEntity(ClientWorld world, com.mojang.authlib.GameProfile profile) {
         super(world, profile);
     }
 
+    private double backUpX, backUpY = -999, backUpZ;
+
     @Override
-    public void resolve(Object mode) {
-        // Kiểm tra FakePlayer để tránh lỗi
-        if ((Object) this == FakePlayer.fakePlayer) {
-            backUpY = -999;
-            return;
-        }
-
-        // Lưu lại vị trí cũ (Backup)
-        backUpX = getX();
-        backUpY = getY();
-        backUpZ = getZ();
-
-        // ĐÃ XÓA SẠCH LOGIC:
-        // Vì bạn đã xóa phần Resolver/Backtrack trong Aura nên không cần tính toán gì ở đây nữa.
-        // Để trống như thế này là an toàn nhất, game sẽ dùng vị trí mặc định của Server.
-    }
-
-        @Override
     public void releaseResolver() {
         if (backUpY != -999) {
             setPosition(backUpX, backUpY, backUpZ);
             backUpY = -999;
         }
     }
+}
