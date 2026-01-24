919c9fa7c7f9a5ec51914353cc648d86a41cd167
duylip789 <duylip555@gmail.com>
2026-01-24 09:50:14 +0700

    Update ClickAction.java

--- src/main/java/thunder/hack/features/modules/player/ClickAction.java
+++ src/main/java/thunder/hack/features/modules/player/ClickAction.java
@@ -7,7 +7,6 @@ import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.util.Hand;
 import thunder.hack.events.impl.EventSync;
 import thunder.hack.features.modules.Module;
-import thunder.hack.setting.Bind;
 import thunder.hack.setting.Setting;
 import thunder.hack.utility.player.InventoryUtility;
 import thunder.hack.utility.player.SearchInvResult;
@@ -18,56 +17,42 @@ public class ClickAction extends Module {
     }
 
     public final Setting<Boolean> pearl = new Setting<>("Pearl", true);
-    public final Setting<Bind> pearlKey = new Setting<>("Pearl Key", new Bind(-1, false, false), v -> pearl.getValue());
+    // Thay đổi Bind thành KeyBind để khớp với core của bạn
+    public final Setting<thunder.hack.setting.KeyBind> pearlKey = new Setting<>("Pearl Key", new thunder.hack.setting.KeyBind(-1), v -> pearl.getValue());
 
     public final Setting<Boolean> xp = new Setting<>("XP", true);
-    public final Setting<Bind> xpKey = new Setting<>("XP Key", new Bind(-1, false, false), v -> xp.getValue());
-
-    public final Setting<Mode> mode = new Setting<>("Mode", Mode.Legit);
-    public final Setting<Boolean> rotation = new Setting<>("Rotation", true);
-
-    public enum Mode { Legit, Fast }
+    public final Setting<thunder.hack.setting.KeyBind> xpKey = new Setting<>("XP Key", new thunder.hack.setting.KeyBind(-1), v -> xp.getValue());
 
     @EventHandler
     public void onSync(EventSync e) {
         if (mc.player == null || mc.world == null) return;
 
-        // Xử lý XP (Giữ nút để đập liên tục)
+        // Xử lý XP: Giữ nút là ném liên tục
         if (xp.getValue() && xpKey.getValue().isKeyDown()) {
-            throwItem(Items.EXPERIENCE_BOTTLE);
+            performSilentAction(Items.EXPERIENCE_BOTTLE);
         }
 
-        // Xử lý Pearl (Bấm nút là ném 1 quả)
-        // Lưu ý: pearlKey.getValue().isKeyPressed() dùng cho nhấn 1 lần
+        // Xử lý Pearl: Nhấn nút là ném 1 quả (dùng chế độ "Just Pressed")
         if (pearl.getValue() && pearlKey.getValue().isKeyPressed()) {
-            throwItem(Items.ENDER_PEARL);
+            performSilentAction(Items.ENDER_PEARL);
         }
     }
 
-    private void throwItem(net.minecraft.item.Item item) {
+    private void performSilentAction(net.minecraft.item.Item item) {
         SearchInvResult result = InventoryUtility.findItemInHotBar(item);
         
         if (result.found()) {
             int oldSlot = mc.player.getInventory().selectedSlot;
-            int newSlot = result.slot();
+            int itemSlot = result.slot();
 
-            if (newSlot != oldSlot) {
-                // Gửi packet đổi slot (Grim/Matrix sẽ thấy bạn cầm item đó rất nhanh)
-                sendPacket(new UpdateSelectedSlotC2SPacket(newSlot));
-                
-                // Gửi packet sử dụng item
-                sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, 0)); // 0 là sequence
-                
-                // Trả về slot cũ ngay lập tức (vẫn cầm kiếm trên màn hình)
+            if (itemSlot != oldSlot) {
+                // Packet-based Silent Switch: Server thấy đổi nhưng tay vẫn cầm kiếm
+                sendPacket(new UpdateSelectedSlotC2SPacket(itemSlot));
+                sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, 0));
                 sendPacket(new UpdateSelectedSlotC2SPacket(oldSlot));
-                
-                // Đồng bộ lại slot cho client tránh bị ghost item
-                mc.player.getInventory().selectedSlot = oldSlot;
             } else {
-                // Nếu đang cầm sẵn trên tay thì chỉ việc ném
                 sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, 0));
             }
         }
     }
 }
-
