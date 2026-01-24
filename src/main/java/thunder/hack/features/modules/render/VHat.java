29cf81fd4e538a46ed066086270e7d47a6abe940
duylip789 <duylip555@gmail.com>
2026-01-24 14:19:55 +0700

    Update VHat.java

--- src/main/java/thunder/hack/features/modules/render/VHat.java
+++ src/main/java/thunder/hack/features/modules/render/VHat.java
@@ -9,13 +9,12 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 import org.joml.Matrix4f;
 import thunder.hack.core.Managers;
-import thunder.hack.events.impl.render.Render3DEvent;
 import thunder.hack.features.modules.Module;
 import thunder.hack.features.modules.client.HudEditor;
 import thunder.hack.setting.Setting;
 import thunder.hack.setting.impl.ColorSetting;
 
-import java.awt.Color;
+import java.awt.*;
 
 public class VHat extends Module {
 
@@ -23,16 +22,27 @@ public class VHat extends Module {
         super("V-Hat", Category.RENDER);
     }
 
-    public final Setting<Boolean> friendsOnly = new Setting<>("Friend", false);
-    public final Setting<Boolean> syncColor = new Setting<>("Sync Color", true);
-    public final Setting<ColorSetting> color =
-            new Setting<>("Color", new ColorSetting(new Color(255, 255, 255, 150).getRGB()),
-                    v -> !syncColor.getValue());
-    public final Setting<Float> scale = new Setting<>("Scale", 0.6f, 0.1f, 1.0f);
-    public final Setting<Float> height = new Setting<>("Height", 0.3f, 0.1f, 1.0f);
+    public final Setting<Boolean> friendsOnly =
+            new Setting<>("Friend", false);
 
-    @EventHandler
-    public void onRender3D(Render3DEvent event) {
+    public final Setting<Boolean> syncColor =
+            new Setting<>("SyncColor", true);
+
+    public final Setting<ColorSetting> color =
+            new Setting<>("Color",
+                    new ColorSetting(new Color(255, 255, 255, 150).getRGB()),
+                    v -> !syncColor.getValue()
+            );
+
+    public final Setting<Float> scale =
+            new Setting<>("Scale", 0.6f, 0.1f, 1.0f);
+
+    public final Setting<Float> height =
+            new Setting<>("Height", 0.3f, 0.1f, 1.0f);
+
+    // ✅ HÀM RENDER ĐÚNG CHO 1.21
+    @Override
+    public void onRender3D(MatrixStack matrices, float tickDelta) {
         if (mc.player == null || mc.world == null) return;
 
         Color finalColor = syncColor.getValue()
@@ -43,29 +53,28 @@ public class VHat extends Module {
             if (player.isInvisible() || player.isSpectator()) continue;
 
             if (friendsOnly.getValue()) {
-                if (player != mc.player && !Managers.FRIEND.isFriend(player.getName().getString()))
+                if (player != mc.player &&
+                        !Managers.FRIEND.isFriend(player.getName().getString()))
                     continue;
             }
 
-            drawHat(event.getMatrixStack(), player, event.getTickDelta(), finalColor);
+            drawHat(matrices, player, tickDelta, finalColor);
         }
     }
 
     private void drawHat(MatrixStack matrices, PlayerEntity player, float tickDelta, Color c) {
         double x = MathHelper.lerp(tickDelta, player.lastRenderX, player.getX())
-                - mc.gameRenderer.getCamera().getPos().x;
+                - mc.gameRenderer.getCamera().getPos().getX();
         double y = MathHelper.lerp(tickDelta, player.lastRenderY, player.getY())
-                - mc.gameRenderer.getCamera().getPos().y;
+                - mc.gameRenderer.getCamera().getPos().getY();
         double z = MathHelper.lerp(tickDelta, player.lastRenderZ, player.getZ())
-                - mc.gameRenderer.getCamera().getPos().z;
+                - mc.gameRenderer.getCamera().getPos().getZ();
 
         float yaw = MathHelper.lerp(tickDelta, player.prevHeadYaw, player.headYaw);
-        float pitch = MathHelper.lerp(tickDelta, player.prevPitch, player.getPitch());
 
         matrices.push();
-        matrices.translate(x, y + player.getHeight(), z);
+        matrices.translate(x, y + player.getHeight() + 0.05, z);
         matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-yaw));
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(pitch));
 
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
@@ -74,33 +83,46 @@ public class VHat extends Module {
         RenderSystem.setShader(GameRenderer::getPositionColorProgram);
 
         Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
         Matrix4f matrix = matrices.peek().getPositionMatrix();
 
-        float radius = scale.getValue();
-        float coneHeight = height.getValue();
+        float r = scale.getValue();
+        float h = height.getValue();
 
-        buffer.vertex(matrix, 0, coneHeight, 0)
+        BufferBuilder buffer = tessellator.begin(
+                VertexFormat.DrawMode.TRIANGLE_FAN,
+                VertexFormats.POSITION_COLOR
+        );
+
+        buffer.vertex(matrix, 0, h, 0)
                 .color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha());
 
         for (int i = 0; i <= 32; i++) {
-            double angle = i * Math.PI * 2 / 32;
-            float px = (float) (Math.cos(angle) * radius);
-            float pz = (float) (Math.sin(angle) * radius);
-            buffer.vertex(matrix, px, 0, pz)
+            double a = i * Math.PI * 2 / 32;
+            buffer.vertex(matrix,
+                            (float) Math.cos(a) * r,
+                            0,
+                            (float) Math.sin(a) * r)
                     .color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha() / 2);
         }
+
         BufferRenderer.drawWithGlobalProgram(buffer.end());
 
         RenderSystem.lineWidth(1.5f);
-        BufferBuilder line = tessellator.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
+
+        BufferBuilder line = tessellator.begin(
+                VertexFormat.DrawMode.LINE_STRIP,
+                VertexFormats.POSITION_COLOR
+        );
+
         for (int i = 0; i <= 32; i++) {
-            double angle = i * Math.PI * 2 / 32;
-            float px = (float) (Math.cos(angle) * radius);
-            float pz = (float) (Math.sin(angle) * radius);
-            line.vertex(matrix, px, 0, pz)
+            double a = i * Math.PI * 2 / 32;
+            line.vertex(matrix,
+                            (float) Math.cos(a) * r,
+                            0,
+                            (float) Math.sin(a) * r)
                     .color(c.getRed(), c.getGreen(), c.getBlue(), 255);
         }
+
         BufferRenderer.drawWithGlobalProgram(line.end());
 
         RenderSystem.enableCull();
