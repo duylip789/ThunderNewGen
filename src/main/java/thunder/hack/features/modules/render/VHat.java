dbc20e1e71c727dde1d01e0e5401cc730ea58d2a
duylip789 <duylip555@gmail.com>
2026-01-24 14:14:24 +0700

    Update VHat.java

--- src/main/java/thunder/hack/features/modules/render/VHat.java
+++ src/main/java/thunder/hack/features/modules/render/VHat.java
@@ -1,20 +1,19 @@
 package thunder.hack.features.modules.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 import org.joml.Matrix4f;
-import thunder.hack.ThunderHack;
 import thunder.hack.core.Managers;
-import thunder.hack.events.impl.render.Render3DEvent; // Đã xóa chữ .render ở giữa
+import thunder.hack.events.impl.render.Render3DEvent;
 import thunder.hack.features.modules.Module;
 import thunder.hack.features.modules.client.HudEditor;
 import thunder.hack.setting.Setting;
 import thunder.hack.setting.impl.ColorSetting;
-import meteordevelopment.orbit.EventHandler;
 
 import java.awt.Color;
 
@@ -26,25 +25,26 @@ public class VHat extends Module {
 
     public final Setting<Boolean> friendsOnly = new Setting<>("Friend", false);
     public final Setting<Boolean> syncColor = new Setting<>("Sync Color", true);
-    public final Setting<ColorSetting> color = new Setting<>("Color", new ColorSetting(new Color(255, 255, 255, 150).getRGB()), v -> !syncColor.getValue());
+    public final Setting<ColorSetting> color =
+            new Setting<>("Color", new ColorSetting(new Color(255, 255, 255, 150).getRGB()),
+                    v -> !syncColor.getValue());
     public final Setting<Float> scale = new Setting<>("Scale", 0.6f, 0.1f, 1.0f);
     public final Setting<Float> height = new Setting<>("Height", 0.3f, 0.1f, 1.0f);
 
     @EventHandler
     public void onRender3D(Render3DEvent event) {
-        if (mc.world == null || mc.player == null) return;
+        if (mc.player == null || mc.world == null) return;
 
-        // Lấy màu sắc
-        Color finalColor = syncColor.getValue() ? HudEditor.getColor(1) : color.getValue().getColorObject();
+        Color finalColor = syncColor.getValue()
+                ? HudEditor.getColor(1)
+                : color.getValue().getColorObject();
 
         for (PlayerEntity player : mc.world.getPlayers()) {
             if (player.isInvisible() || player.isSpectator()) continue;
 
-            // Kiểm tra Friend
             if (friendsOnly.getValue()) {
-                boolean isMe = (player == mc.player);
-                boolean isFriend = Managers.FRIEND.isFriend(player.getName().getString());
-                if (!isMe && !isFriend) continue;
+                if (player != mc.player && !Managers.FRIEND.isFriend(player.getName().getString()))
+                    continue;
             }
 
             drawHat(event.getMatrixStack(), player, event.getTickDelta(), finalColor);
@@ -52,10 +52,12 @@ public class VHat extends Module {
     }
 
     private void drawHat(MatrixStack matrices, PlayerEntity player, float tickDelta, Color c) {
-        // Nội suy vị trí để không bị giật
-        double x = MathHelper.lerp(tickDelta, player.lastRenderX, player.getX()) - mc.gameRenderer.getCamera().getPos().getX();
-        double y = MathHelper.lerp(tickDelta, player.lastRenderY, player.getY()) - mc.gameRenderer.getCamera().getPos().getY();
-        double z = MathHelper.lerp(tickDelta, player.lastRenderZ, player.getZ()) - mc.gameRenderer.getCamera().getPos().getZ();
+        double x = MathHelper.lerp(tickDelta, player.lastRenderX, player.getX())
+                - mc.gameRenderer.getCamera().getPos().x;
+        double y = MathHelper.lerp(tickDelta, player.lastRenderY, player.getY())
+                - mc.gameRenderer.getCamera().getPos().y;
+        double z = MathHelper.lerp(tickDelta, player.lastRenderZ, player.getZ())
+                - mc.gameRenderer.getCamera().getPos().z;
 
         float yaw = MathHelper.lerp(tickDelta, player.prevHeadYaw, player.headYaw);
         float pitch = MathHelper.lerp(tickDelta, player.prevPitch, player.getPitch());
@@ -72,33 +74,34 @@ public class VHat extends Module {
         RenderSystem.setShader(GameRenderer::getPositionColorProgram);
 
         Tessellator tessellator = Tessellator.getInstance();
-        
-        // Vẽ thân nón
         BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
         Matrix4f matrix = matrices.peek().getPositionMatrix();
+
         float radius = scale.getValue();
         float coneHeight = height.getValue();
 
-        buffer.vertex(matrix, 0, coneHeight, 0).color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha());
+        buffer.vertex(matrix, 0, coneHeight, 0)
+                .color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha());
 
         for (int i = 0; i <= 32; i++) {
-            double angle = i * 2 * Math.PI / 32;
+            double angle = i * Math.PI * 2 / 32;
             float px = (float) (Math.cos(angle) * radius);
             float pz = (float) (Math.sin(angle) * radius);
-            buffer.vertex(matrix, px, 0, pz).color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha() / 2);
+            buffer.vertex(matrix, px, 0, pz)
+                    .color(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha() / 2);
         }
         BufferRenderer.drawWithGlobalProgram(buffer.end());
 
-        // Vẽ viền nón
         RenderSystem.lineWidth(1.5f);
-        BufferBuilder lineBuffer = tessellator.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
+        BufferBuilder line = tessellator.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
         for (int i = 0; i <= 32; i++) {
-            double angle = i * 2 * Math.PI / 32;
+            double angle = i * Math.PI * 2 / 32;
             float px = (float) (Math.cos(angle) * radius);
             float pz = (float) (Math.sin(angle) * radius);
-            lineBuffer.vertex(matrix, px, 0, pz).color(c.getRed(), c.getGreen(), c.getBlue(), 255);
+            line.vertex(matrix, px, 0, pz)
+                    .color(c.getRed(), c.getGreen(), c.getBlue(), 255);
         }
-        BufferRenderer.drawWithGlobalProgram(lineBuffer.end());
+        BufferRenderer.drawWithGlobalProgram(line.end());
 
         RenderSystem.enableCull();
         RenderSystem.depthMask(true);
